d3789320e3991c81b25ba030677c87c7eb73d215
lucasbernardo-git <lucas0930cabral@gmail.com>
2025-06-18 18:46:02 -0300

    Definição das rotas de empresa.py

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import uvicorn
 from fastapi import FastAPI
 from database import engine, Base
-from app.routers import empresa
+#from app.routers import empresa
+from app.routers.empresa import router as empresa_router
 from app.models import cargo, cliente, copias_livro, empresa, emprestimo, enderecos, funcionario, livro, pessoas
 
 app = FastAPI()
@@ -15,7 +16,7 @@ Base.metadata.create_all(bind=engine)
 def check_api():
     return "Api Online!"
 
-
+app.include_router(empresa_router)
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="127.0.0.1", port=5000, reload=True)
